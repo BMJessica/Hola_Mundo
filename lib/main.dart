9a113e0fac684350b8e3272e91182e7ccea66a50
BMJessica <jessicaboxtha@gmail.com>
2025-09-07 21:22:48 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hola Mundo',
+      home: Center(child: Text('Hola Mundo')),
+    );
+  }
+}
